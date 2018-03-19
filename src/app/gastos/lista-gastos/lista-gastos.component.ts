16d7325fded9639a3386944298048237b39127e9
wilgustavo <wilgustavo@gmail.com>
2018-03-19 19:09:02 -0300

    llenar lista

--- src/app/gastos/lista-gastos/lista-gastos.component.ts
+++ src/app/gastos/lista-gastos/lista-gastos.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaGastosComponent implements OnInit {
 
+  lista;
+
   constructor() { }
 
   ngOnInit() {
+    this.llenarLista();
+  }
+
+  private llenarLista() {
+    this.lista = [
+      {
+        id: 1,
+        categoria: 'Transporte',
+        importe: 321.98,
+        fecha: '2018-03-19'
+      },
+      {
+        id: 2,
+        categoria: 'Salud',
+        importe: 1140.55,
+        fecha: '2018-02-08'
+      },
+      {
+        id: 3,
+        categoria: 'Alimentación',
+        importe: 705.22,
+        fecha: '2018-03-18'
+      }
+    ];
   }
 
 }
